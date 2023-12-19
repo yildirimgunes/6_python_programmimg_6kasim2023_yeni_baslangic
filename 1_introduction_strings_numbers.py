d0f8eabc3dea8decfb3e4d39715dfb2ec9208ecf
yildirimgunes <yildirimgunes1973@gmail.com>
2023-12-19 11:04:36 +0300

    1a

---
+++ 1_introduction_strings_numbers.py
@@ -0,0 +1,76 @@
+#introduction_strings_numbers
+#integers
+a=type(9)
+print(a)
+#floats
+b=type(9.2)
+print(b)
+#string
+print (type("hello ai era"))
+
+#DETAILS_OF_STRINGS
+123
+type(123)
+"123"
+"A"+"b"
+"a"+"b"+"c"
+"a" "b"
+"a" " b"
+"a" "_b"
+
+"a "*3
+#STRING_METHODS - len()
+c="python_lessons"
+c=10
+a=5
+
+d="python_lessons"
+e="python_lessons"
+#it is advised that after using function del, put  symbol #  in front of it
+#del e
+len(d)
+#STRING METHODS- upper and lower
+#only first letter of word will be capital
+e.capitalize()
+#all letters of word will be capital
+e.upper()
+f="PYTHOn"
+f.lower()
+e.isupper()
+e.islower()
+
+#STRıNG METHODS - Replace
+e.replace("p", "r")
+print(e)
+#STRING METHODS-strip
+d.strip()
+d="*python_lessons*"
+d.strip("*")
+#strıng methods
+dir(e)
+dir(str)
+e.casefold()
+e.capitalize()
+e.title()
+#SUBSTRINGS
+e[0:3]
+#DEGISKENLER
+a=87999
+k=3
+l=a*k
+l/3
+type(k)
+type(121)
+type (12.3)
+type(2+9j)
+#TYPE TRANSFORMATION
+number1=input()
+number2=input()
+type(number1)
+number1+number2
+int(number1)+int(number2)
+m=number1+number2
+print ("1","2")
+print("python", "lessons", sep="_")
+type()
+?print()
