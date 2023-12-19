a50c32f918d691179a678e6a3654f34372277c0e
yildirimgunes <yildirimgunes1973@gmail.com>
2023-12-19 10:59:42 +0300

    python_vidoe_denemeleri

---
+++ 6_definition_function.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# # functions_usually _rguments_of_function_in_
+# paranthesis_are_
+# seperated_by_comma
+print()
+print()
+len("a")
+#mathematical_operations
+5*5
+5/5
+10-1
+10+1
+2**2
+2**3
+
+x=5
+
+def squareanumber(x):
+    print(x**2)
+
+
+
+def numbers(x):
+    print(x**3)
+
+
+
+def numberplus(x):
+    print(x+4)
+
+numberplus(9)
+
+
+def numbersdouble(x,y):
+    z=((x+y)*2)
+    print("The_numbers_given:" + str(x) + " and "+ str(y))
+    print("Square_of_two_numbers:" + str(z))
+
+numbersdouble(4,5)
+
